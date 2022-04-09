2b1cc95d708c9d3929b684615b45959757af9413
Javier Verde Marín <73306559+javVM@users.noreply.github.com>
2022-04-09 19:02:42 +0200

    Delete cliente/src directory

--- cliente/src/constantFile.js
+++
@@ -1,6 +0,0 @@
-
-export const INFURA_URL='https://rinkeby.infura.io/v3/54dc25092d254c6ebb823949b6a4b9db';
-export const SERVER_URL='http://localhost:3001';
-export const CONTRACT_ADDRESS='0xaF3105f24B6dca8dC31d8562806C5a98997D038f';
-export const PRIVATE_KEY= '0x43ba79a72ee8a09796f15a7aca065bc55b1625cbc9ada3ef94beab8816ac89ef';
-export const ADDRESS = '0x74edbc557d7c13a084623a2c0aa7f1fcaeb89b9d';
