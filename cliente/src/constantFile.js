d534dd7c3f1aee04f843f6675c0e9b724c4d72b8
Alejandro Ramirez Rodriguez <70910303+rzAlejandro@users.noreply.github.com>
2022-05-09 20:11:32 +0200

    Delete constantFile.js

--- cliente/src/constantFile.js
+++
@@ -1,7 +0,0 @@
-export const INFURA_URL='https://rinkeby.infura.io/v3/54dc25092d254c6ebb823949b6a4b9db';
-export const PROJECT_ID = '54dc25092d254c6ebb823949b6a4b9db';
-export const PROJECT_SECRET = '0e320e2d105c4dc9a9c808eaf59eb549';
-export const SERVER_URL='http://localhost:3001';
-export const CONTRACT_ADDRESS='0xF4bA49ff04214BBC3f2b80901C2A2DD316399627'; //0xE77bE944132c0eac50f1a200D2F3816ab0F08853
-export const PRIVATE_KEY= '0xbaddb5fc5cf1f466ff9067c1f41b0cf05beb66e9e5a9fc44f187f25cc5064ee7';
-export const ADDRESS = '0x7115c57f213Ac4e97D76f8C29aa100A91E4Ebb10';
