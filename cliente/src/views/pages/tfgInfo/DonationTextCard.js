0ee424464bd3a79578f395c3f0c219d80c7a1714
Javier Verde Marín <73306559+javVM@users.noreply.github.com>
2022-05-23 17:04:45 +0200

    Update DonationTextCard.js

--- cliente/src/views/pages/tfgInfo/DonationTextCard.js
+++ cliente/src/views/pages/tfgInfo/DonationTextCard.js
@@ -22,7 +22,81 @@ const CardWrapper = styled(MainCard)(({ theme }) => ({
 // ===========================|| DASHBOARD DEFAULT - EARNING CARD ||=========================== //
 
 const DonationTextCard = ({ isLoading }) => {
+    const theme = useTheme();/* eslint-disable */
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+// material-ui
+import { styled, useTheme } from '@mui/material/styles';
+import { Avatar, Box, Grid, Menu, MenuItem, Typography } from '@mui/material';
+import {  Header } from 'semantic-ui-react'
+
+// project imports
+import MainCard from 'ui-component/cards/MainCard';
+import SkeletonEarningCard from 'ui-component/cards/Skeleton/EarningCard';
+
+
+const CardWrapper = styled(MainCard)(({ theme }) => ({
+    backgroundColor: theme.palette.secondary.dark,
+    color: '#fff',
+    overflow: 'hidden',
+    position: 'relative'
+}));
+
+// ===========================|| DASHBOARD DEFAULT - EARNING CARD ||=========================== //
+
+const DonationTextCard = ({ isLoading }) => {
     const theme = useTheme();
+
+
+
+    return (
+        <>
+            {isLoading ? (
+                <SkeletonEarningCard />
+            ) : (
+                <CardWrapper border={false} content={false}>
+                    <Box sx={{ p: 4.25 }}>
+                        <Grid container direction="column">
+                            <Grid item>
+                                <Header as='h1' style={{ "color" : "white" }}>¿Por qué donar?</Header>
+                            </Grid>
+                            
+                            <Grid item xs={12}>
+                                <Typography 
+                                    component = {'span'}
+                                    sx={{
+                                        fontSize: '1rem',
+                                        fontWeight: 500,
+                                        color: '#fff'
+                                    }}
+                                    align='left'
+                                    margin = {12}
+                                >
+                                    <p></p>
+                                    
+                                    <p color= '#3949ab'>
+                                        Publicar la denuncia en la Blockchain no es gratis. Con tu ayuda estás dando voz a los denunciantes.
+                                    
+                                    </p>
+                                    
+                                </Typography>
+                            </Grid>
+                            
+                            
+                        </Grid>
+                    </Box>
+                </CardWrapper>
+            )}
+        </>
+    );
+};
+
+DonationTextCard.propTypes = {
+    isLoading: PropTypes.bool
+};
+
+export default DonationTextCard;
 
 
 
