56a9d0825a448b2fde1ada576783ff0c12e57848
Alejandro Ramirez Rodriguez <70910303+rzAlejandro@users.noreply.github.com>
2022-05-30 13:49:20 +0200

    Delete createWallet.js

--- smart_contract/createWallet.js
+++
@@ -1,7 +0,0 @@
-//Creamos una cartera de ethereum
-const wallet = require('ethereumjs-wallet');
-//Generamos las claves públicas y privadas
-const addressData = wallet['default'].generate();
-//Mostramos la clave privada y la direccion
-console.log(`Private key = , ${addressData.getPrivateKeyString()}`);
-console.log(`Address = , ${addressData.getAddressString()}`);
